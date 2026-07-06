829d7a9f572a78c55781dce2cfd55be2e05e3419
strxkyy <gabrielfreire615@gmail.com>
2026-07-05 23:23:09 -0300

    criacao de rota de login e rota base do site assim como sistema de login usando banco .db local

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import sqlite3 #banco de dados embutido 
-from flask import Flask,redirect,render_template,get_flashed_messages  #flask e funçoes necessarias
+from flask import Flask,redirect,render_template,get_flashed_messages, request #flask e funçoes necessarias
 
 con = sqlite3.connect("usuario.db")
 
@@ -11,9 +11,33 @@ con.commit()
 
 site = Flask(__name__)
 
-@site.route("/") # rota de entrada inicial/verificação do site 
-def verificacao_usuario(): #necessario definir uma função,necessaria para retornar o comando "render_template" que ira executar nosso arquivo .html na pasta templates
-    return render_template("verificacao.html")
+@site.route("/", methods=["get","post"])
+def login():
+    return render_template("login.html")
+
+@site.route("/verificacao", methods=["get","post"]) 
+def verificacao_usuario(): 
+    nome = request.form.get("nome")
+    senha = request.form.get("senha")
+
+    con = sqlite3.connect("usuario.db")
+    cursor = con.cursor()
+
+    cursor.execute("select * from usuario where nome = ? and senha = ?",
+    (nome,senha))
+
+    usuario = cursor.fetchone()
+
+    con.close()
+
+    if usuario:
+    
+     return render_template("login.html")
+    
+    else:
+    
+     return render_template("home.html") 
+
 
 site.run(debug=True)
 
